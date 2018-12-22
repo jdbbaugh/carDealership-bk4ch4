b4234033119adcb866b590afc8f8b7d9afbb57d2
jdbbaugh <jbaugh78@msn.com>
2018-12-21 20:03:42 -0500

    realized profit number included years other then 2017 -must fix this

--- src/scripts/dealer.js
+++ src/scripts/dealer.js
@@ -4,14 +4,16 @@ import dealerList from "./dealerList"
 const dealer = {
   addProfit(fromDealer) {
     // const profitSum = fromDealer.gross_profit.reduce((total, amount) => {total +amount});
+    // console.log(fromDealer);
     let addProfit = 0
     fromDealer.forEach(info => {
       addProfit += info.gross_profit
+      console.log(info.purchase_date)
     })
     // console.log(addProfit);
     let profitContainer = document.createElement("h2");
     profitContainer.setAttribute("class", "profit-container");
-    profitContainer.textContent = `Profit Total = ${addProfit}`;
+    profitContainer.textContent = `2017 Profit Total = ${addProfit}`;
     return profitContainer;
   },
   mostSellsMonth () {
